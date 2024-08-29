55b6edafc1c28f1637d511487b0365f2233a64f7
Christian <christian75876@gmail.com>
2024-08-28 21:07:20 -0500

    Add entity request

--- src/modules/database.module.ts
+++ src/modules/database.module.ts
@@ -1,6 +1,7 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { DatabaseConfigService } from "src/common/config/connection-db.config";
+import { RequestModule } from './request/request.module';
 
 
 @Module({
@@ -8,6 +9,7 @@ import { DatabaseConfigService } from "src/common/config/connection-db.config";
         TypeOrmModule.forRootAsync({
             useClass: DatabaseConfigService
         }),
+        RequestModule,
     ],
     providers: [DatabaseConfigService,],
     exports: [DatabaseConfigService]

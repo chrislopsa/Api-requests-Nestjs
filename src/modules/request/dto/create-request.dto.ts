b3ba400c27e591659ada9d6120ad24262d74c806
AlejandroRivas <a.rivaslopez020@gmail.com>
2024-08-30 20:14:32 -0500

    create query services find all and modified entity

--- src/modules/request/dto/create-request.dto.ts
+++ src/modules/request/dto/create-request.dto.ts
@@ -1,32 +1,31 @@
-import { IsNotEmpty, IsNumber } from "class-validator";
+import { IsNotEmpty, IsNumber } from 'class-validator';
 
 export class CreateRequestDto {
-    @IsNotEmpty({message: 'id_property is required'})
-    @IsNumber({}, {message: 'Id is a number'})
-    id_property: number;
+  @IsNotEmpty({ message: 'id_property is required' })
+  @IsNumber({}, { message: 'Id is a number' })
+  id_property: number;
 
-    @IsNotEmpty({ message: 'id_client is required' })
-    @IsNumber({}, { message: 'Id is a number' })
-    id_client: number;
+  @IsNotEmpty({ message: 'id_client is required' })
+  @IsNumber({}, { message: 'Id is a number' })
+  id_client: number;
 
-    @IsNotEmpty({ message: 'id_state is required' })
-    @IsNumber({}, { message: 'Id is a number' })
-    id_state: number;
+  @IsNotEmpty({ message: 'id_state is required' })
+  @IsNumber({}, { message: 'Id is a number' })
+  id_state: number;
 
-    @IsNotEmpty({ message: 'id_requestType is required' })
-    @IsNumber({}, { message: 'Id is a number' })
-    id_requestType: number;
+  @IsNotEmpty({ message: 'id_requestType is required' })
+  @IsNumber({}, { message: 'Id is a number' })
+  id_requestType: number;
 
-    @IsNotEmpty({ message: 'id_owner is required' })
-    @IsNumber({}, { message: 'Id is a number' })
-    id_owner: number;
+  @IsNotEmpty({ message: 'id_owner is required' })
+  @IsNumber({}, { message: 'Id is a number' })
+  id_owner: number;
 
-    @IsNotEmpty({ message: 'id_role is required' })
-    @IsNumber({}, { message: 'Id is a number' })
-    id_role: number
+  @IsNotEmpty({ message: 'id_role is required' })
+  @IsNumber({}, { message: 'Id is a number' })
+  id_role: number;
 
-    @IsNotEmpty({ message: 'date is required' })
-    @IsNumber({}, { message: 'Id is a number' })
-    date: string
-
+  @IsNotEmpty({ message: 'date is required' })
+  @IsNumber({}, { message: 'Id is a number' })
+  date: string;
 }
--- src/modules/request/services/create.service.ts
+++ src/modules/request/services/create.service.ts
@@ -1,15 +1,15 @@
-import { Injectable } from "@nestjs/common";
-import { InjectRepository } from "@nestjs/typeorm";
-import { Request } from "../entities/request.entity";
-import { Repository } from "typeorm";
-import { CreateRequestDto } from "../dto/create-request.dto";
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Request } from '../entities/request.entity';
+import { Repository } from 'typeorm';
+import { CreateRequestDto } from '../dto/create-request.dto';
 
 @Injectable()
 export class Create {
-    constructor(
-        @InjectRepository(Request) private requestRepository: Repository<Request>,
-    ){};
-    async create(requestData: CreateRequestDto): Promise<Request>{
-        return
-    }
-};
+  constructor(
+    @InjectRepository(Request) private requestRepository: Repository<Request>,
+  ) {}
+  async create(requestData: CreateRequestDto): Promise<Request> {
+    return;
+  }
+}
